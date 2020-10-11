3881803ad030782835efb46130055933b36495bc
Chong ZHOU <wanghu.zc@alibaba-inc.com>
2020-10-11 12:00:35 +0800

    Transform unsigned long to ASCII string for assert log

--- src/kernel/kernel_panic.c
+++ src/kernel/kernel_panic.c
@@ -12,10 +12,59 @@ base_no_return _kernel_panic(
     SCREEN_HEIGHT - 1, 0);
 
   while (1) {
-    // This allows the CPU to enter a sleep state in which it consumes much
-    // less energy. See: https://en.wikipedia.org/wiki/HLT_(x86_instruction)
     __asm__("hlt");
   }
+}
+
+base_private void _unsigned_to_str(usz_t uval, char *buf, usz_t buf_cap)
+{
+  usz_t buf_off;
+  usz_t digit_cnt;
+  usz_t uval_origin = uval;
+
+  /* Counting digits in total*/
+  digit_cnt = 0;
+  do {
+    uval /= 10;
+    digit_cnt++;
+  } while(uval > 0);
+
+  /* No enough space, output 'ER' to denote an error to be noticed. */
+  if((digit_cnt + 1) > buf_cap) {
+    if (buf_cap >= 3) {
+      buf[0] = 'E';
+      buf[1] = 'R';
+      buf[2] = '\0';
+    } else if (buf_cap == 2) {
+      buf[0] = 'E';
+      buf[1] = '\0';
+    } else if (buf_cap == 1) {
+      /* buf_cap equals 1, only output an char E here to denote an error.
+         NULL terminator can not be included as we think to 
+         discover an error is more important than elegant display. */
+      buf[0] = 'E';
+    } else {
+      /* buf_cap must equals 0, we can do nothing overwriting no memory */
+    }
+    return;
+  }
+
+  uval = uval_origin;
+  buf_off = digit_cnt - 1;
+  while (true) {
+    usz_t digit;
+
+    digit = uval % 10;
+    buf[buf_off] = (ch_t)(digit + '0');
+
+    buf_off--;
+    uval /= 10;
+    if (uval == 0) {
+      break;
+    }
+  }
+
+  buf[digit_cnt] = '\0';
 }
 
 base_no_return _kernel_assert_fail(const char *expr, const char *file, usz_t line)
@@ -30,15 +79,11 @@ base_no_return _kernel_assert_fail(const char *expr, const char *file, usz_t lin
   screen_write_str("File:", SCREEN_COLOR_WHITE, SCREEN_COLOR_RED, row++, 0);
   screen_write_str(file, SCREEN_COLOR_WHITE, SCREEN_COLOR_RED, row++, 0);
 
-  /*
-  itoa(line, ln_str, 10);
-  screen_write_str("Line no.:", SCREEN_COLOR_WHITE, SCREEN_COLOR_RED, row++, 0);
+  _unsigned_to_str(line, ln_str, 128);
+  screen_write_str("Line:", SCREEN_COLOR_WHITE, SCREEN_COLOR_RED, row++, 0);
   screen_write_str(ln_str, SCREEN_COLOR_WHITE, SCREEN_COLOR_RED, row++, 0);
-  */
 
   while (1) {
-    // This allows the CPU to enter a sleep state in which it consumes much
-    // less energy. See: https://en.wikipedia.org/wiki/HLT_(x86_instruction)
     __asm__("hlt");
   }
 }
