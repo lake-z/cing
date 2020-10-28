7934e6104f095188ae3b76584f2fc34c965d583d
Chong ZHOU <wanghu.zc@alibaba-inc.com>
2020-10-28 11:24:37 +0800

    Finish hooking all interrupts to ISRs
    Specific ISRs needs to be implemented.

--- src/interrupts/interrupts.c
+++ src/interrupts/interrupts.c
@@ -8,6 +8,44 @@ typedef enum {
   IDT_GATE_TYPE_TRAP
 } idt_gate_type_t;
 
+/* Actually a pointer to the stack, CPU will first push related registers as
+ * on stack as arguments to ISR, see:
+ * https://os.phil-opp.com/cpu-exceptions/#the-interrupt-stack-frame 
+ *
+ * And then, all registers are pushed onto stack and to be restored later by
+ * isr_common_stub or irq_common_stub. 
+ * 
+ * And finally note that stack grows downward. */
+typedef byte_t intr_args_t;
+
+typedef enum {
+  INTR_ID_EX_FAULT_DE = 0,
+  INTR_ID_EX_FAULT_TRAP_DB = 1,
+  INTR_ID_EX_INTERRUPT_NMI = 2,
+  INTR_ID_EX_TRAP_BP = 3,
+  INTR_ID_EX_TRAP_OF = 4,
+  INTR_ID_EX_FAULT_BR = 5,
+  INTR_ID_EX_FAULT_UD = 6,
+  INTR_ID_EX_FAULT_NM = 7,
+  INTR_ID_EX_ABORT_DF = 8,
+  /* 9 is not deprecated. */
+  INTR_ID_EX_FAULT_TS = 10,
+  INTR_ID_EX_FAULT_NP = 11,
+  INTR_ID_EX_FAULT_SS = 12,
+  INTR_ID_EX_FAULT_GP = 13,
+  INTR_ID_EX_FAULT_PF = 14,
+  /* 15 is reserved. */
+  INTR_ID_EX_FAULT_MF = 16,
+  INTR_ID_EX_FAULT_AC = 17,
+  INTR_ID_EX_ABORT_MC = 18,
+  INTR_ID_EX_FAULT_XF = 19,
+  INTR_ID_EX_FAULT_VE = 20,
+  /* 21..29 is reserved. */
+  INTR_ID_EX_SX = 30,
+  /* 31 is reserved. */
+  INTR_ID_MAX /* End token, not a valid interrupt ID */
+} intr_id_t;
+
 extern void isr0(void);
 extern void isr1(void);
 extern void isr2(void);
@@ -49,9 +87,6 @@ extern void irq4(void);
 #define IDT_GATE_LEN 16
 #define IDT_GATE_COUNT 256
 
-/* Mapping IRQ 0..15 to interrupt handler index 32..32+15 */
-base_private const byte_t IRQ_HANDLER_BASE = 32;
-
 /*
  * Code segement initialiezd as the second entey of GDT.
  *
@@ -73,6 +108,26 @@ base_private const u16_t GDT_CODE_SEGMENT_OFFSET = 0x08;
 
 /* Interrupt Descriptor Table, has 256 gates, and 18 bytes len each */
 base_private byte_t _idt[IDT_GATE_LEN * IDT_GATE_COUNT];
+
+/* 
+ ******************************************************************************
+                    ____________                          ____________
+Real Time Clock -> |            |   Timer -------------> |            |
+ACPI ------------> |            |   Keyboard-----------> |            |
+Available -------> | Secondary  |----------------------> | Primary    |     C
+Available -------> | Interrupt  |   Serial Port 2 -----> | Interrupt  |---> P
+Mouse -----------> | Controller |   Serial Port 1 -----> | Controller |     U
+Co-Processor ----> |            |   Parallel Port 2/3 -> |            |
+Primary ATA -----> |            |   Floppy disk -------> |            |
+Secondary ATA ---> |____________|   Parallel Port 1----> |____________|
+
+ * ****************************************************************************
+ */
+ /* The default configuration of the PICs is not usable, because it sends 
+  * interrupt vector numbers in the range 0–15 to the CPU. These numbers are 
+  * already occupied by CPU exceptions.
+  * Mapping IRQ 0..15 to interrupt handler index 32..32+15 */
+base_private const byte_t IRQ_HANDLER_BASE = 32;
 
 base_private const byte_t *_idt_gate_encode(
   usz_t gate_idx,
@@ -218,8 +273,13 @@ void intr_init(void)
 
 void intr_isr_handler(u64_t id, uptr_t stack_addr)
 {
+  intr_id_t iid;
+
+  kernel_assert(id < INTR_ID_MAX);
+  iid = (intr_id_t)id;
+  
   kernel_panic("TODO: intr_isr_handler");
-  (void)(id + stack_addr);
+  (void)(iid + stack_addr);
 }
 
 void intr_irq_handler(u64_t id, uptr_t stack_addr)
