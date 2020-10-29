fbc6b294676f5fa34fdd01a7f66d5debb4b0e04d
Chong ZHOU <wanghu.zc@alibaba-inc.com>
2020-10-29 13:40:58 +0800

    Output assert fail info at the benath of screen
    So we can save more information about what is happening before
    assertion failure.

--- src/kernel/kernel_panic.c
+++ src/kernel/kernel_panic.c
@@ -76,19 +76,18 @@ base_no_return _kernel_panic(
 
 base_no_return _kernel_assert_fail(const char *expr, const char *file, usz_t line)
 {
-  usz_t row = 0;
   char ln_str[128];
   screen_write_str("Kernel assertion failure!", SCREEN_COLOR_WHITE, SCREEN_COLOR_RED, 
-    row++, 0);
+    SCREEN_HEIGHT - 7, 0);
   screen_write_str("Expression:", SCREEN_COLOR_WHITE, SCREEN_COLOR_RED, 
-    row++, 0);
-  screen_write_str(expr, SCREEN_COLOR_WHITE, SCREEN_COLOR_RED, row++, 0);
-  screen_write_str("File:", SCREEN_COLOR_WHITE, SCREEN_COLOR_RED, row++, 0);
-  screen_write_str(file, SCREEN_COLOR_WHITE, SCREEN_COLOR_RED, row++, 0);
+    SCREEN_HEIGHT - 6, 0);
+  screen_write_str(expr, SCREEN_COLOR_WHITE, SCREEN_COLOR_RED, SCREEN_HEIGHT - 5, 0);
+  screen_write_str("File:", SCREEN_COLOR_WHITE, SCREEN_COLOR_RED, SCREEN_HEIGHT - 4, 0);
+  screen_write_str(file, SCREEN_COLOR_WHITE, SCREEN_COLOR_RED, SCREEN_HEIGHT - 3, 0);
 
   _unsigned_to_str(line, ln_str, 128);
-  screen_write_str("Line:", SCREEN_COLOR_WHITE, SCREEN_COLOR_RED, row++, 0);
-  screen_write_str(ln_str, SCREEN_COLOR_WHITE, SCREEN_COLOR_RED, row++, 0);
+  screen_write_str("Line:", SCREEN_COLOR_WHITE, SCREEN_COLOR_RED, SCREEN_HEIGHT - 2, 0);
+  screen_write_str(ln_str, SCREEN_COLOR_WHITE, SCREEN_COLOR_RED, SCREEN_HEIGHT - 1, 0);
 
   while (1) {
     __asm__("hlt");
