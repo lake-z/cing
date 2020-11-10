453e453a853e8a61e959198eb7de10671b1faa8d
Chong ZHOU <wanghu.zc@alibaba-inc.com>
2020-11-10 22:42:28 +0800

    List all signatures of SDTs

--- src/drivers/drivers_acpi.c
+++ src/drivers/drivers_acpi.c
@@ -3,17 +3,32 @@
 #include "kernel_panic.h"
 #include "log.h"
 
+typedef struct sdt_header {
+  ch_t signature[4];
+  u32_t len;
+  u8_t revision;
+  u8_t checksum;
+  ch_t oemid[6];
+  ch_t oem_tab_id[8];
+  u32_t oem_revision;
+  u32_t creator_id;
+  u32_t creator_revision;
+} base_struct_packed sdt_header_t;
+
 base_private void _init_rsdt(byte_t *rsdt)
 {
-  base_private const usz_t _MSG_CAP = 128;
-  ch_t msg[_MSG_CAP];
-  // const ch_t *msg_part;
-  usz_t msg_len;
+  sdt_header_t *sdt = (sdt_header_t *)rsdt;
+  usz_t entry_cnt = (sdt->len - sizeof(sdt_header_t)) / 4;
+ 
+  log_info_len(sdt->signature, 4);
+  log_info_len(sdt->oemid, 6);
+  log_info_len(sdt->oem_tab_id, 6);
 
-  msg_len = 0;
-  msg_len += str_buf_marshal_str(msg, msg_len, _MSG_CAP, (const ch_t *)rsdt, 4);
-  msg_len += str_buf_marshal_terminator(msg, msg_len, _MSG_CAP);
-  log_info_len(msg, msg_len);
+  for (usz_t i = 0; i < entry_cnt; i++) {
+    u32_t ptr32 = *(u32_t *)(rsdt + sizeof(sdt_header_t) + i * 4);
+    sdt = (sdt_header_t *)(uptr_t)ptr32;
+    log_info_len(sdt->signature, 4);
+  }
 }
 
 void acpi_init(const byte_t *multi_boot_info, usz_t len)
