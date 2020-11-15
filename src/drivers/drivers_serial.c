bd8283435d0f1ef449b42eec6b0e0408822d102b
Chong ZHOU <wanghu.zc@alibaba-inc.com>
2020-11-15 15:16:26 +0800

    Refactor drivers_serial port management

--- src/drivers/drivers_serial.c
+++ src/drivers/drivers_serial.c
@@ -6,63 +6,31 @@ base_private const u16_t _SPEED_115200_BAUDS = 1;
 base_private const u16_t _SPEED_57600_BAUDS = 2;
 base_private const u16_t _SPEED_38400_BAUDS = 3;
 
-/*
- * ****************************************************************************
- * All the I/O ports are calculated relative to the data port. This is because
- * all serial ports (COM1, COM2, COM3, COM4) have their ports in the same
- * order, but they start at different values.
- * ****************************************************************************
- */
-base_private port_no_t _port_data(port_no_t base)
-{
-  return base;
-}
-
-base_private port_no_t _port_fifo_cmd(port_no_t base)
-{
-  return base + 2;
-}
-
-base_private port_no_t _port_line_cmd(port_no_t base)
-{
-  return base + 3;
-}
-
-base_private port_no_t _port_modem_cmd(port_no_t base)
-{
-  return base + 4;
-}
-
-base_private port_no_t _port_line_status(port_no_t base)
-{
-  return base + 5;
-}
-
 void serial_init(void)
 {
   /* Enable DLAB */
-  port_write_byte(_port_line_cmd(PORT_NO_SERIAL_COM1), 0x80);
+  port_write_byte(PORT_NO_SERIAL_COM1_CMD_LINE, 0x80);
 
-  port_write_byte(_port_data(PORT_NO_SERIAL_COM1), byte_get(_SPEED_115200_BAUDS, 1));
-  port_write_byte(_port_data(PORT_NO_SERIAL_COM1), byte_get(_SPEED_115200_BAUDS, 0));
+  port_write_byte(PORT_NO_SERIAL_COM1_DATA, byte_get(_SPEED_115200_BAUDS, 1));
+  port_write_byte(PORT_NO_SERIAL_COM1_DATA, byte_get(_SPEED_115200_BAUDS, 0));
 
   // cf. https://littleosbook.github.io/#configuring-the-serial-port
   // Bit:     | 7 | 6 | 5 4 3 | 2 | 1 0 |
   // Content: | d | b | prty  | s | dl  |
   // Value:   | 0 | 0 | 0 0 0 | 0 | 1 1 | = 0x03
-  port_write_byte(_port_line_cmd(PORT_NO_SERIAL_COM1), 0x03);
+  port_write_byte(PORT_NO_SERIAL_COM1_CMD_LINE, 0x03);
 
   // Enable FIFO, clear them, with 14b threshold
-  port_write_byte(_port_fifo_cmd(PORT_NO_SERIAL_COM1), 0xc7);
+  port_write_byte(PORT_NO_SERIAL_COM1_CMD_FIFO, 0xc7);
 
   // IRQs enabled, RTS/DSR set
-  port_write_byte(_port_modem_cmd(PORT_NO_SERIAL_COM1), 0x0B);
+  port_write_byte(PORT_NO_SERIAL_COM1_CMD_MODEM, 0x0B);
 }
 
 base_private bo_t _fifo_transmit_is_empty(void)
 {
   /* 0x20 = 0010 0000 */
-  byte_t data = port_read_byte(_port_line_status(PORT_NO_SERIAL_COM1));
+  byte_t data = port_read_byte(PORT_NO_SERIAL_COM1_STATUS_LINE);
   return (data & 0x20);
 }
 
--- src/include/drivers_port.h
+++ src/include/drivers_port.h
@@ -16,6 +16,11 @@ typedef enum {
   PORT_NO_VGA_ATTRIBUTE_WRITE = 0x3c0,
   PORT_NO_VGA_ATTRIBUTE_READ = 0x3c1,
   PORT_NO_SERIAL_COM1 = 0x3f8,
+  PORT_NO_SERIAL_COM1_DATA = 0x03f8 + 1,
+  PORT_NO_SERIAL_COM1_CMD_FIFO = 0x03f8 + 2,
+  PORT_NO_SERIAL_COM1_CMD_LINE = 0x03f8 + 3,
+  PORT_NO_SERIAL_COM1_CMD_MODEM = 0x03f8 + 4,
+  PORT_NO_SERIAL_COM1_STATUS_LINE = 0x03f8 + 5,
 } port_no_t;
 
 void port_write_byte(port_no_t port, byte_t data);
